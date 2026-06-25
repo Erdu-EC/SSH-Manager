34d8700abaf9380ca342fd18c8f9e8dc066aad80
eduar <eduardocastilloh1999@gmail.com>
2026-06-25 15:41:26 -0600

    Refactor app structure: remove unused components (AppLogo, TemplateMenu), rename app to "SSH Explorer," simplify layout, update dependencies, enhance Nuxt configuration with layers and modules, and integrate i18n and Vitest.

--- ssh-manager/src/app/app.config.ts
+++ ssh-manager/src/app/app.config.ts
@@ -2,7 +2,17 @@ export default defineAppConfig({
   ui: {
     colors: {
       primary: 'green',
-      neutral: 'slate'
+      neutral: 'zinc'
+    },
+    button: {
+      defaultVariants: {
+        size: 'sm'
+      }
+    },
+    icons: {
+      chevronDown: 'i-lucide-chevron-down',
+      folder: 'i-lucide-folder',
+      folderOpen: 'i-lucide-folder-open'
     }
   }
 })
--- ssh-manager/src/nuxt.config.ts
+++ ssh-manager/src/nuxt.config.ts
@@ -1,13 +1,30 @@
 export default defineNuxtConfig({
+  extends: [
+    './layers/core',
+    './layers/dashboard',
+    './layers/explorer',
+    './layers/terminal'
+  ],
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/test-utils/module'
   ],
 
   ssr: false,
+
+  imports: {
+    dirs: [
+      'layers/core/stores',
+      'layers/explorer/stores',
+      'layers/terminal/stores',
+      'layers/dashboard/stores'
+    ]
+  },
 
   devtools: {
     enabled: true
@@ -20,7 +37,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { prerender: true }
   },
-  compatibilityDate: '2026-06-22',
+  compatibilityDate: '2026-06-25',
 
   vite: {
     clearScreen: false,
@@ -31,7 +48,10 @@ export default defineNuxtConfig({
     optimizeDeps: {
       include: [
         '@tauri-apps/api/core',
+        '@tauri-apps/api/event',
         '@tauri-apps/api/webview',
+        '@xterm/addon-fit',
+        '@xterm/xterm',
         'pinia'
       ]
     }
@@ -45,5 +65,35 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
+  },
+
+  i18n: {
+    lazy: true,
+    langDir: 'locales/',
+    restructureDir: 'lang',
+    locales: [
+      { code: 'en', file: 'en.json' },
+      { code: 'es', file: 'es.json' }
+    ],
+    defaultLocale: 'en',
+    strategy: 'no_prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      alwaysRedirect: true,
+      fallbackLocale: 'en'
+    },
+    bundle: {
+      optimizeTranslationDirective: false
+    }
+  },
+
+  pinia: {
+    storesDirs: [
+      './layers/core/stores',
+      './layers/explorer/stores',
+      './layers/terminal/stores',
+      './layers/dashboard/stores'
+    ]
   }
 })
